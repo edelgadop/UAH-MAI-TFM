dae6090d99bf58fbf98b2dfebfcedb42d46a140d
Emilio Delgado Pascual <emdp.91@gmail.com>
2022-03-26 18:00:39 +0100

    Build streamlit app

--- main.py
+++
@@ -1,9 +0,0 @@
-from preprocessing.preprocessing import extract_images
-
-
-IMG_DIR = "../images"
-FILE_IMDB = "../imdb_crop.tar"
-FILE_WIKI = "../wiki_crop.tar"
-
-if __name__ == "__main__":
-    print("Hello!")
---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.title("Age and gender detection using CNNs")
